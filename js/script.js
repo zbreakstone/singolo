b13a6f20dcd917926edd74e0f6a771514860e436
Zack Breakstone <z.breakstone@gmail.com>
2015-01-26 02:21:00 -0500

    Build portfolio section; Add jquery and portfolio nav function

--- js/script.js
+++ js/script.js
@@ -1,1 +1,13 @@
 // This is the main javascript file for the Singolo Project
+
+$(document).ready(function(){
+	// When a portfolio nav list item is clicked...
+	$("nav.portfolio-nav > ul > li").click(function(){
+		// Toggle the "active" class for its <a> child
+		$(this).children("a").toggleClass("active");
+		// Then remove the "active" class from any siblings' <a> child
+		$(this).siblings().children("a").removeClass("active");
+	});
+});
+
+
